010d884683e49e4c01d16dc44303cb840fe78e22
volth <volth@volth.com>
2016-09-05 16:37:46 +0000

    build scripts

---
+++ compile-deb--xfce4-terminal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -f /.dockerenv ]; then
+   echo "This script better to run in dockerized environment"
+   ~/docker/u16.sh $0
+   exit
+fi
+
+sudo apt-get update
+sudo apt-get install   -y dpkg-dev devscripts xfce4-dev-tools
+sudo apt-get build-dep -y xfce4-terminal
+
+sudo sed -i -r 's/disable-maintainer-mode/enable-maintainer-mode/g' /usr/share/perl5/Debian/Debhelper/Buildsystem/autoconf.pm
+
+pushd xfce4-terminal-*
+debchange --nmu xxx
+debuild clean
+debuild -i -us -uc -b
+popd
+
+# here .deb can be installed and tested
+#dpkg -i ...
+#/bin/bash
---
+++ follow-apt-source.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO:
+
+#git checkout follow-apt-source
+#git rm -rf xfce4-terminal*
+#apt-get source xfce4-terminal
+#git add xfce4-terminal-*
+#git commit -a -m xfce4-terminal
+# ... merge with master here
+#git checkout master
+
